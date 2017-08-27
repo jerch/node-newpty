fa818979d7562515a98918b085bd3e75c8e4521d
Jörg Breitbart <jerch@rockborn.de>
2017-08-27 18:50:19 +0200

    fix solaris

--- src/pty.test.ts
+++ src/pty.test.ts
@@ -31,7 +31,7 @@ describe('native functions', () => {
     it('apply termios settings (slave only)', () => {
         // TODO
     });
-    it('set_size (master only)', () => {
+    it('set_size', () => {
         let master: number = -1;
         let slave: number = -1;
         assert.doesNotThrow(() => {
@@ -54,10 +54,23 @@ describe('native functions', () => {
         });
         assert.equal(size.cols, 12);
         assert.equal(size.rows, 13);
+        if (process.platform !== 'sunos') {
+            size = {cols: -1, rows: -1};
+            assert.doesNotThrow(() => {
+                size = pty.set_size(slave, 23, 24);
+            });
+            assert.equal(size.cols, 23);
+            assert.equal(size.rows, 24);
+            assert.doesNotThrow(() => {
+                size = pty.get_size(slave);
+            });
+            assert.equal(size.cols, 23);
+            assert.equal(size.rows, 24);
+        }
         fs.closeSync(master);
         fs.closeSync(slave);
     });
-    it('get_size (master and slave)', () => {
+    it('get_size', () => {
         let master: number = -1;
         let slave: number = -1;
         assert.doesNotThrow(() => {
@@ -66,16 +79,21 @@ describe('native functions', () => {
             pty.unlockpt(master);
             slave = fs.openSync(pty.ptsname(master), fs.constants.O_RDWR | fs.constants.O_NOCTTY);
             pty.load_driver(slave);
+            pty.set_size(master, 12, 13);
         });
         let size_master: Interfaces.ISize = {cols: -1, rows: -1};
         let size_slave: Interfaces.ISize = {cols: -1, rows: -1};
+        let size: Interfaces.ISize = {cols: 12, rows: 13};
         assert.doesNotThrow(() => {
             size_master = pty.get_size(master);
         });
-        assert.doesNotThrow(() => {
-            size_slave = pty.get_size(slave);
-        });
-        assert.deepEqual(size_master, size_slave);
+        assert.deepEqual(size_master, size);
+        if (process.platform !== 'sunos') {
+            assert.doesNotThrow(() => {
+                size_slave = pty.get_size(slave);
+            });
+            assert.deepEqual(size_slave, size);
+        }
         fs.closeSync(master);
         fs.closeSync(slave);
     });
