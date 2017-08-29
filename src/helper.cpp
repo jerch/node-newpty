1d30aec63c13c9fc149ac7f285724be3ef8e9ee4
Jörg Breitbart <jerch@rockborn.de>
2017-08-30 00:02:38 +0200

    removed redundant solaris code

--- src/helper.cpp
+++ src/helper.cpp
@@ -12,20 +12,12 @@
 
 #if defined(SOLARIS)
 #include <termios.h>
-#include <stropts.h>
 #endif
 
 int main(int argc, char *argv[]) {
 #if defined(TIOCSCTTY)
     if (ioctl(STDIN_FILENO, TIOCSCTTY, NULL) == -1)
         return -1;
-#elif defined(I_PUSH) && defined(I_FIND)
-        if (ioctl(STDIN_FILENO, I_FIND, "ptem") == 0) {
-                ioctl(STDIN_FILENO, I_PUSH, "ptem");
-        }
-        if (ioctl(STDIN_FILENO, I_FIND, "ldterm") == 0) {
-                ioctl(STDIN_FILENO, I_PUSH, "ldterm");
-        }
 #endif
     execvp(argv[1], &argv[1]);
     return errno;
